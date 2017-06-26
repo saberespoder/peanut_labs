138c1e2faf0aab01dfa4923d8b8af89a8ccbb858
Alex Timofeev <atimofeev@reactant.ru>
2017-06-26 11:53:17 +0700

    Validate `public_user_id` alphanumericality

--- spec/peanut_labs/builder/user_id_spec.rb
+++ spec/peanut_labs/builder/user_id_spec.rb
@@ -2,19 +2,18 @@
 require 'spec_helper'
 
 describe PeanutLabs::Builder::UserId do
+  # Following documentation lead: http://peanut-labs.github.io/publisher-doc/#iframe-getuserid
   let(:user_id) { 'user1' }
   let(:labs_id) { '0000' }
   let(:app_key) { 'd755913ed731c335656a9578be648aa0' }
 
-  it 'should return something' do
-    PeanutLabs::Credentials.id = labs_id
+  it 'should return end user id' do
+    PeanutLabs::Credentials.id  = labs_id
     PeanutLabs::Credentials.key = app_key
 
-    iframe_id = PeanutLabs::Builder::UserId.new(user_id).call.split('-')
+    end_user_id = PeanutLabs::Builder::UserId.new(user_id).call
 
-    expect(iframe_id[0]).to eql user_id
-    expect(iframe_id[1]).to eql labs_id
-    expect(iframe_id[2]).to eql 'aa3ad22725'
+    expect(end_user_id).to eq('user1-0000-aa3ad22725')
   end
 
   it 'errors out if credentials are missing' do
@@ -24,5 +23,11 @@ describe PeanutLabs::Builder::UserId do
     expect{
       PeanutLabs::Builder::UserId.new('random').call
     }.to raise_error PeanutLabs::CredentialsMissingError
+  end
+
+  it 'errors out if public ID is not alphanumeric' do
+    expect{
+      PeanutLabs::Builder::UserId.new('123~@~abc').call
+    }.to raise_error PeanutLabs::UserIdAlphanumericError
   end
 end
