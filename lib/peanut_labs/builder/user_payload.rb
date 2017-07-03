a8af1d73095c0be4821333ab32fa4e83aafc65cc
Alex Timofeev <atimofeev@reactant.ru>
2017-07-03 17:41:58 +0700

    Make sure that generated payload is decryptable

--- lib/peanut_labs/builder/user_payload.rb
+++ lib/peanut_labs/builder/user_payload.rb
@@ -11,13 +11,17 @@ module PeanutLabs
       # It turns out that we don't have add AES padding inside `encrypt_json_payload`
       # because unlike PHP implementation OpenSSL applies them itself
 
-      def self.call(payload={})
+      def self.call(payload = {}, init_vector = nil)
         json_payload = payload.to_json
-        init_vector  = get_init_vector
+        init_vector  = get_init_vector unless init_vector
 
         return {
+          # @TODO: Fetch application key from credentials
           iv:      init_vector,
-          payload: encrypt_json_payload(json_payload, 'abcdef0123456789abcdef0123456789', 'LB4Fs5awRnFYoLm%2BMTcANg%3D%3D')
+          payload: encrypt_json_payload(json_payload,
+            'abcdef0123456789abcdef0123456789',
+            init_vector
+          )
         }
       end
 
@@ -32,15 +36,15 @@ module PeanutLabs
         # Convert application key into binary format
         binary_key = [application_key].pack('H*')
         # Encrypt payload
-        encrypted_payload = encrypt_payload(payload, binary_key, iv)
+        encrypted_payload = encryptor(payload, binary_key, iv)
 
         CGI.escape(Base64.strict_encode64(encrypted_payload))
       end
 
-      def self.encrypt_payload(payload, application_key, iv)
+      def self.encryptor(payload, key, iv)
         cipher     = OpenSSL::Cipher.new(ENCRYPTION).encrypt
-        cipher.key = application_key
         cipher.iv  = Base64.strict_decode64(CGI.unescape(iv))
+        cipher.key = key
 
         cipher.update(payload) + cipher.final
       end
--- spec/peanut_labs/builder/user_payload_spec.rb
+++ spec/peanut_labs/builder/user_payload_spec.rb
@@ -1,33 +1,64 @@
 require 'spec_helper'
 
 describe PeanutLabs::Builder::UserPayload do
-  let(:app_key) { 'abcdef0123456789abcdef0123456789' }
+  let(:app_key)     { 'abcdef0123456789abcdef0123456789' }
+  let(:init_vector) { nil } # Serves testing purposes and will not be passed in the real life
   let(:data) do
     {
-      user_id: 'user001-1001-2389d74882',
-      cc: 'US',
-      sex: 1,
-      dob: '1990-04-10',
-      postal: '94104',
-      profile_data: {
-        q122: ['qx122-0'],
-        q159: ['qx159-1'],
-        q102: ['qx102-105'],
-        q158: ['qx158-3'],
-        q101: ['qx101-2'],
-        q157: ['3'],
+      'user_id' => 'user001-1001-2389d74882',
+      'cc'      => 'US',
+      'sex'     => 1,
+      'dob'     => '1990-04-10',
+      'postal'  => '94104',
+      'profile_data' => {
+        'q122' => ['qx122-0'],
+        'q159' => ['qx159-1'],
+        'q102' => ['qx102-105'],
+        'q158' => ['qx158-3'],
+        'q101' => ['qx101-2'],
+        'q157' => ['3'],
         'ch-m' => ['4-2004'],
         'ch-f' => ['10-1998', '11-1999']
       }
     }
   end
-  subject { PeanutLabs::Builder::UserPayload.call(data) }
+  subject { PeanutLabs::Builder::UserPayload.call(data, init_vector) }
 
-  it 'iv attribute has expected length' do
-    expect(CGI.unescape(subject[:iv]).length).to eq 24
+  it 'has expected attributes' do
+    expect(subject.keys).to include(:iv, :payload)
   end
 
-  it 'returns encrypted payload' do
-    expect(subject[:payload]).to eq 'aVJyjog7mQrtUMUDZONYOAlSqBRa1%2BoMIhZhH6Qahs8hvWSflN7s7Z98fR8QyeqfdQ8blnp5x3B3IbuwwNQBORTfXRJ0lKWytvXa%2BUK%2FUCYOFPSvasgGIKVtolrHy7n6vJujtbpC3IKJxnWRbOtKV5Q5xw9Vz6hgzvhf%2FwNV3m9N0qkMxFon%2FNEu933mbQOEjgwswjWHjFGQ5%2BugTzCT5PX47nDnV8vXR014KCZcjGw8DtkGog5A%2B%2B4SdBbEOP0M9g165ZQ5nsFtyjy8960bRGef7UNey4hmoFiFvqlws%2BAhs7YAT%2Fb7c97K6amSCZm9TocPG4tJAqZYgoHTCApFgIuGbvfnlt%2B0%2FPSehNluyl8%3D'
+  context 'init vector' do
+    it 'always unique' do
+      first_call  = subject
+      second_call = PeanutLabs::Builder::UserPayload.call(data)
+      expect(first_call[:iv]).not_to eq(second_call[:iv])
+    end
+    it 'has expected length' do
+      expect(CGI.unescape(subject[:iv]).length).to eq 24
+    end
+  end
+
+  context 'payload' do
+    context 'with defined vector' do
+      let(:init_vector) { 'LB4Fs5awRnFYoLm%2BMTcANg%3D%3D' }
+      it 'returns expected value' do
+        expect(subject[:payload]).to eq 'aVJyjog7mQrtUMUDZONYOAlSqBRa1%2BoMIhZhH6Qahs8hvWSflN7s7Z98fR8QyeqfdQ8blnp5x3B3IbuwwNQBORTfXRJ0lKWytvXa%2BUK%2FUCYOFPSvasgGIKVtolrHy7n6vJujtbpC3IKJxnWRbOtKV5Q5xw9Vz6hgzvhf%2FwNV3m9N0qkMxFon%2FNEu933mbQOEjgwswjWHjFGQ5%2BugTzCT5PX47nDnV8vXR014KCZcjGw8DtkGog5A%2B%2B4SdBbEOP0M9g165ZQ5nsFtyjy8960bRGef7UNey4hmoFiFvqlws%2BAhs7YAT%2Fb7c97K6amSCZm9TocPG4tJAqZYgoHTCApFgIuGbvfnlt%2B0%2FPSehNluyl8%3D'
+      end
+    end
+
+    context 'with random vector' do
+      let(:iv)      { Base64.strict_decode64(CGI.unescape(subject[:iv])) }
+      let(:payload) { Base64.strict_decode64(CGI.unescape(subject[:payload])) }
+
+      it 'can be decrypted' do
+        decipher          = OpenSSL::Cipher.new('AES-128-CBC').decrypt
+        decipher.iv       = iv
+        decipher.key      = [app_key].pack('H*')
+        decrypted_payload = decipher.update(payload) + decipher.final
+
+        expect(JSON.parse(decrypted_payload)).to eq data
+      end
+    end
   end
 end
