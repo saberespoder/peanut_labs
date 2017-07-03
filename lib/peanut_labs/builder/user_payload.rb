475ae0a939d4faf56b56f0808269eb9ad9d22dfc
Alex Timofeev <atimofeev@reactant.ru>
2017-07-03 16:05:11 +0700

    Reproduce PHP implementation of payload encryptor

--- lib/peanut_labs/builder/user_payload.rb
+++ lib/peanut_labs/builder/user_payload.rb
@@ -1,22 +1,48 @@
 require 'openssl'
 require 'base64'
+require 'json'
 
 module PeanutLabs
   module Builder
     class UserPayload
-      IV_SIZE = 16.freeze
+      ENCRYPTION = 'AES-128-CBC'.freeze
+      BLOCK_SIZE = 16.freeze
+
+      # It turns out that we don't have add AES padding inside `encrypt_json_payload`
+      # because unlike PHP implementation OpenSSL applies them itself
 
       def self.call(payload={})
-        iv = get_init_vector
-        { iv: iv }
+        json_payload = payload.to_json
+        init_vector  = get_init_vector
+
+        return {
+          iv:      init_vector,
+          payload: encrypt_json_payload(json_payload, 'abcdef0123456789abcdef0123456789', 'LB4Fs5awRnFYoLm%2BMTcANg%3D%3D')
+        }
       end
 
       private
 
       def self.get_init_vector
-        cipher    = OpenSSL::Cipher.new('AES-128-CBC')
-        random_iv = cipher.random_iv
-        CGI.escape(Base64.strict_encode64(random_iv))
+        cipher = OpenSSL::Cipher.new(ENCRYPTION).encrypt
+        CGI.escape(Base64.strict_encode64(cipher.random_iv))
+      end
+
+      def self.encrypt_json_payload(payload, application_key, iv)
+        # Convert application key into binary format
+        binary_key = [application_key].pack('H*')
+        # Encrypt payload
+        encrypted_payload = encrypt_payload(payload, binary_key, iv)
+
+        CGI.escape(Base64.strict_encode64(encrypted_payload))
+      end
+
+      def self.encrypt_payload(payload, application_key, iv)
+        cipher     = OpenSSL::Cipher.new(ENCRYPTION).encrypt
+        cipher.key = application_key
+        cipher.iv  = Base64.strict_decode64(CGI.unescape(iv))
+
+        cipher.update(payload) + cipher.final
       end
     end
   end
--- spec/peanut_labs/builder/user_payload_spec.rb
+++ spec/peanut_labs/builder/user_payload_spec.rb
@@ -1,10 +1,33 @@
 require 'spec_helper'
 
 describe PeanutLabs::Builder::UserPayload do
-  subject { PeanutLabs::Builder::UserPayload }
-  let(:payload) { subject.call }
+  let(:app_key) { 'abcdef0123456789abcdef0123456789' }
+  let(:data) do
+    {
+      user_id: 'user001-1001-2389d74882',
+      cc: 'US',
+      sex: 1,
+      dob: '1990-04-10',
+      postal: '94104',
+      profile_data: {
+        q122: ['qx122-0'],
+        q159: ['qx159-1'],
+        q102: ['qx102-105'],
+        q158: ['qx158-3'],
+        q101: ['qx101-2'],
+        q157: ['3'],
+        'ch-m' => ['4-2004'],
+        'ch-f' => ['10-1998', '11-1999']
+      }
+    }
+  end
+  subject { PeanutLabs::Builder::UserPayload.call(data) }
 
-  it "returns proper iv attribute" do
-    expect(CGI.unescape(payload[:iv]).length).to eq 24
+  it 'iv attribute has expected length' do
+    expect(CGI.unescape(subject[:iv]).length).to eq 24
+  end
+
+  it 'returns encrypted payload' do
+    expect(subject[:payload]).to eq 'aVJyjog7mQrtUMUDZONYOAlSqBRa1%2BoMIhZhH6Qahs8hvWSflN7s7Z98fR8QyeqfdQ8blnp5x3B3IbuwwNQBORTfXRJ0lKWytvXa%2BUK%2FUCYOFPSvasgGIKVtolrHy7n6vJujtbpC3IKJxnWRbOtKV5Q5xw9Vz6hgzvhf%2FwNV3m9N0qkMxFon%2FNEu933mbQOEjgwswjWHjFGQ5%2BugTzCT5PX47nDnV8vXR014KCZcjGw8DtkGog5A%2B%2B4SdBbEOP0M9g165ZQ5nsFtyjy8960bRGef7UNey4hmoFiFvqlws%2BAhs7YAT%2Fb7c97K6amSCZm9TocPG4tJAqZYgoHTCApFgIuGbvfnlt%2B0%2FPSehNluyl8%3D'
   end
 end
